e53031b1d69b3e9cee8d1f822ae1762b764411b2
jeanmariemcc <84250957+jeanmariemcc@users.noreply.github.com>
2022-09-27 13:47:14 -0400

    Add files via upload
    Initial blockchain,  produces genesis block.

---
+++ Blockchain/Backend/core/blockchain.py
@@ -0,0 +1,37 @@
+import sys
+sys.path.append('/Users/jeanm/OneDrive/Documents/Udemy-Bitcoin')
+
+from Blockchain.Backend.core.block import Block
+from Blockchain.Backend.core.blockheader import BlockHeader
+from Blockchain.Backend.util.util import hash256
+import time
+import json
+
+ZERO_HASH = '0' * 64
+VERSION = 1
+
+class Blockchain:
+    def __init__(self):
+        self.chain = []
+        self.GenesisBlock()
+        
+    def GenesisBlock(self):
+        BlockHeight = 0
+        prevBlockHash = ZERO_HASH
+        self.addBlock(BlockHeight, prevBlockHash)
+        
+    def addBlock(self, BlockHeight, prevBlockHash):
+        timestamp = int(time.time())
+        Transaction = f"Codies Alert sent {BlockHeight} Bitcoins to Jeo"
+        merkleRoot = hash256(Transaction.encode()).hex()
+        bits = 'ffff001f'
+        blockheader = BlockHeader(VERSION, prevBlockHash, merkleRoot, timestamp, bits)
+        blockheader.mine()
+        self.chain.append(Block(BlockHeight, 1, blockheader.__dict__, 1, Transaction).__dict__)
+        print(json.dumps(self.chain, indent = 4))
+        
+                
+        
+if __name__ == "__main__":
+    blockchain = Blockchain()
+    
